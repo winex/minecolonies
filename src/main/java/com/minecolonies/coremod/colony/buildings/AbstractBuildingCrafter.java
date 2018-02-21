40408bf22f0e4719a99611d13f19e41776b0974b
Raycoms <ray.neiheiser@gmail.com>
2018-02-21 16:47:42 +0000

    allow 10x more recipes for specific crafters

--- src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuildingCrafter.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/AbstractBuildingCrafter.java
@@ -3,23 +3,23 @@ package com.minecolonies.coremod.colony.buildings;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.minecolonies.api.colony.requestsystem.resolver.IRequestResolver;
+import com.minecolonies.api.colony.requestsystem.token.IToken;
 import com.minecolonies.api.util.constant.TypeConstants;
-import com.minecolonies.blockout.views.Window;
-import com.minecolonies.coremod.client.gui.WindowHutWorkerPlaceholder;
-import com.minecolonies.coremod.colony.CitizenData;
 import com.minecolonies.coremod.colony.Colony;
 import com.minecolonies.coremod.colony.ColonyView;
-import com.minecolonies.coremod.colony.jobs.AbstractJob;
-import com.minecolonies.coremod.colony.jobs.JobSawmill;
 import com.minecolonies.coremod.colony.requestsystem.resolvers.PublicWorkerCraftingRequestResolver;
 import net.minecraft.util.math.BlockPos;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Class of the sawmill building.
  */
 public abstract class AbstractBuildingCrafter extends AbstractBuildingWorker
 {
+    /**
+     * Extra amount of recipes the crafters can learn.
+     */
+    private static final int EXTRA_RECIPE_MULTIPLIER = 10;
+
     /**
      * Instantiates a new sawmill building.
      *
@@ -44,10 +44,53 @@ public abstract class AbstractBuildingCrafter extends AbstractBuildingWorker
         return builder.build();
     }
 
+    @Override
+    public boolean canRecipeBeAdded(final IToken token)
+    {
+        return AbstractBuildingCrafter.canBuildingCanLearnMoreRecipes (getBuildingLevel(), super.getRecipes().size());
+    }
 
     @Override
     public boolean canCraftComplexRecipes()
     {
         return true;
+    }
+
+    /**
+     * BuildingDeliveryman View.
+     */
+    public static class View extends AbstractBuildingWorker.View
+    {
+
+        /**
+         * Instantiate the deliveryman view.
+         *
+         * @param c the colonyview to put it in
+         * @param l the positon
+         */
+        public View(final ColonyView c, final BlockPos l)
+        {
+            super(c, l);
+        }
+
+        /**
+         * Check if an additional recipe can be added.
+         * @return true if so.
+         */
+        public boolean canRecipeBeAdded()
+        {
+            return AbstractBuildingCrafter.canBuildingCanLearnMoreRecipes (getBuildingLevel(), super.getRecipes().size());
+        }
+    }
+
+    /**
+     * Check if an additional recipe can be added.
+     * @param learnedRecipes the learned recipes.
+     * @param buildingLevel the building level.
+     * @return true if so.
+     */
+    public static boolean canBuildingCanLearnMoreRecipes(final int buildingLevel, final int learnedRecipes)
+    {
+        return (Math.pow(2, buildingLevel) * EXTRA_RECIPE_MULTIPLIER) >= (learnedRecipes + 1);
     }
 }
--- src/main/java/com/minecolonies/coremod/colony/buildings/BuildingSawmill.java
+++ src/main/java/com/minecolonies/coremod/colony/buildings/BuildingSawmill.java
@@ -1,12 +1,16 @@
 package com.minecolonies.coremod.colony.buildings;
 
+import com.minecolonies.api.colony.requestsystem.token.IToken;
+import com.minecolonies.api.crafting.IRecipeStorage;
 import com.minecolonies.blockout.views.Window;
 import com.minecolonies.coremod.client.gui.WindowHutWorkerPlaceholder;
 import com.minecolonies.coremod.colony.CitizenData;
 import com.minecolonies.coremod.colony.Colony;
+import com.minecolonies.coremod.colony.ColonyManager;
 import com.minecolonies.coremod.colony.ColonyView;
 import com.minecolonies.coremod.colony.jobs.AbstractJob;
 import com.minecolonies.coremod.colony.jobs.JobSawmill;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.NotNull;
 
@@ -57,10 +61,32 @@ public class BuildingSawmill extends AbstractBuildingCrafter
         return SAWMILL;
     }
 
+    @Override
+    public boolean canRecipeBeAdded(final IToken token)
+    {
+        if(!super.canRecipeBeAdded(token))
+        {
+            return false;
+        }
+
+        final IRecipeStorage storage = ColonyManager.getRecipeManager().getRecipes().get(token);
+        if(storage == null)
+        {
+            return false;
+        }
+
+        for(final ItemStack stack : storage.getInput())
+        {
+
+        }
+        
+        return true;
+    }
+
     /**
      * BuildingDeliveryman View.
      */
-    public static class View extends AbstractBuildingWorker.View
+    public static class View extends AbstractBuildingCrafter.View
     {
 
         /**
